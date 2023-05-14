3d4c0580ea8d609f97b7e712b54a9cc407553d45
Mohammed Baskhanov <baskhanov.mr@mail.ru>
2023-05-14 12:04:40 +0300

    Fix PEP8, add validator

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from .validators import validate_year
+
+
 class Genre(models.Model):
     """Модель Genre."""
     name = models.CharField(max_length=256)
     slug = models.SlugField(unique=True)
-    
+
     def __str__(self):
         return self.name[:40]
 
@@ -21,10 +24,17 @@ class Category(models.Model):
 class Title(models.Model):
     """Модель Title."""
     name = models.CharField(max_length=256)
-    year = models.IntegerField()
-    description = models.CharField(max_length=1024, blank=True, null=True)
-    genre = models.ManyToManyField(Genre)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    year = models.IntegerField(validators=[validate_year])
+    description = models.CharField(max_length=1024, blank=True)
+    genre = models.ManyToManyField(Genre, related_name='titles')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 blank=True,
+                                 null=True,
+                                 related_name='titles')
+
+    class Meta:
+        ordering = ['year']
 
     def __str__(self):
         return self.name[:40]
