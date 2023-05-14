53fe82ae252892cab4a43aa2db020324f46943af
Михаил Миноцкий <smoke-m@yandex.com>
2023-05-14 18:42:53 +0300

    настроил свои модельки, и юзера

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from .validators import validate_year
+
+
+class User(AbstractUser):
+    """Модель юзера."""
+    ROLE = [
+        ('user', 'пользователь'),
+        ('moderator', 'модератор'),
+        ('admin', 'админ'),
+    ]
+    email = models.EmailField(max_length=254, unique=True)
+    bio = models.TextField(blank=True)
+    role = models.CharField(max_length=13, choices=ROLE, default='user')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['username', 'email'],
+                name='unique_username_email',
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.username} {self.email} {self.role}'
 
 
 class Genre(models.Model):
@@ -38,3 +63,60 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name[:40]
+
+
+class Reviews(models.Model):
+    """Модель отзывов с оценкой на произведение."""
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    text = models.TextField(max_length=200)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    score = models.IntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(10, 'Не так много! 10 это max!'),
+            MinValueValidator(1, 'хоть 1 поставь')
+        ]
+    )
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_author_title'
+            )
+        ]
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text[:40]
+
+
+class Comments(models.Model):
+    """Модель комментариев к отзывам."""
+    review = models.ForeignKey(
+        Reviews,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    text = models.TextField(max_length=200)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text
