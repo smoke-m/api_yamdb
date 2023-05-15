66ce0bdaf2ae2f665c1191633df9200018826c93
Михаил Миноцкий <smoke-m@yandex.com>
2023-05-15 17:04:43 +0300

    serializers, views - done

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from reviews.models import Category, Genre, Title
+
+from reviews.models import Category, Comments, Genre, Reviews, Title
 
 
 class TitleSerializer(serializers.ModelSerializer):
@@ -35,3 +36,36 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['name', 'slug']
         read_only_fields = ['slug']
+
+
+class ReviewsSerializer(serializers.ModelSerializer):
+    """Сериализатор для отзыввов."""
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True)
+    score = serializers.IntegerField(max_value=10, min_value=1)
+
+    class Meta:
+        model = Reviews
+        fields = ['id', 'text', 'author', 'score', 'pub_date']
+        read_only = ['id']
+
+    def validate(self, data):
+        request = self.context['request']
+        title = self.context['view'].kwargs.get('title_id')
+        if request.method == 'POST':
+            if Reviews.objects.filter(author=request.user,
+                                      title=title).exists():
+                raise serializers.ValidationError(
+                    'Ваш отзыв уже есть.')
+        return data
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    """Сериализатор для комментарие."""
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True)
+
+    class Meta:
+        model = Comments
+        fields = ['id', 'text', 'author', 'pub_date']
+        read_only = ['id']
